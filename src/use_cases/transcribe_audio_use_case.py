f2c6a182ac69fb4552ff231116e939c5c9d9be07
AgustinMadygraf <agustin.mtto.madygraf>
2025-09-29 18:06:27 -0300

    Refactor SpeechToTextGateway to implement SpeechToTextPort interface; update TranscribeAudioUseCase to use SpeechToTextPort for improved abstraction.

--- src/use_cases/transcribe_audio_use_case.py
+++ src/use_cases/transcribe_audio_use_case.py
@@ -2,14 +2,21 @@
 Path: src/use_cases/transcribe_audio_use_case.py
 """
 
-from src.interface_adapter.gateways.speech_to_text_gateway import SpeechToTextGateway
+from abc import ABC, abstractmethod
 from src.entities.transcription import Transcription
+
+class SpeechToTextPort(ABC):
+    "Puerto para servicios de transcripción de audio a texto."
+    @abstractmethod
+    def transcribe(self, audio_file_path: str) -> Transcription:
+        "Transcribe el archivo de audio y devuelve una entidad Transcription."
+        pass #pylint disable=unnecessary-pass
 
 class TranscribeAudioUseCase:
     "Caso de uso para transcribir archivos de audio a texto."
-    def __init__(self, speech_gateway: SpeechToTextGateway):
-        self.speech_gateway = speech_gateway
+    def __init__(self, speech_service: SpeechToTextPort):
+        self.speech_service = speech_service
 
     def execute(self, audio_file_path: str) -> Transcription:
         "Transcribe el archivo de audio y devuelve una entidad Transcription."
-        return self.speech_gateway.transcribe(audio_file_path)
+        return self.speech_service.transcribe(audio_file_path)
